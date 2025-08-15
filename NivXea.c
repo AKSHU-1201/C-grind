a65d83cd9e0238e9c8df2cd7b7b23f6909d1e7da
NivXea <akshaysingh1201@gmail.com>
2025-08-15 22:18:51 +0530

    printf("Hello, World!")

--- NivXea.c
+++ NivXea.c
@@ -1,1 +1,1 @@
-So I'm learning this language through CodeWithHarry , he is one of the best teachers out there, as I already know Python Logic Doing this will be easy and fast..
+So I'm learning this language through CodeWithHarry , he is one of the best teachers out there, as I already know Python Logic Doing this will be easy and fast.
